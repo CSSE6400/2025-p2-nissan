92ec4d8de35a8419c71ea76beb08f2d8df0ecc02
Nissan Dookeran <12583+nissan@users.noreply.github.com>
2025-03-25 09:43:33 +1000

    fix github actions compilation issue

--- todo/__init__.py
+++ todo/__init__.py
@@ -8,18 +8,17 @@ def create_app(config_overrides=None):
     if config_overrides:
         app.config.update(config_overrides)
     
-    # Load the models
+    # Initialize SQLAlchemy
     from todo.models import db
-    from todo.models.todo import Todo
     db.init_app(app)
+    
+    # Register the blueprints
+    from todo.views.routes import api
+    app.register_blueprint(api)
     
     # Create the database tables
     with app.app_context():
         db.create_all()
         db.session.commit()
-        
-    # Register the blueprints
-    from todo.views.routes import api
-    app.register_blueprint(api)
 
     return app
